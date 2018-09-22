61dd977b49eb770c4efb0b871678598fa20a006f
Diogo Correia <me@diogotc.com>
2018-09-22 22:57:22 +0100

    First commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import Home from "./components/home.js";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Home />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/home.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import { Elevation } from "@rmwc/elevation";
+
+class Home extends Component {
+  render() {
+    return (
+      <div id="home">
+        <Elevation z="3" id="resource-input">
+          test
+        </Elevation>
+      </div>
+    );
+  }
+}
+
+export default Home;
