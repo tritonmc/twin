a0907b1d9fc9548d1ce373e4009d406edba7ddfb
Diogo Correia <me@diogotc.com>
2019-03-31 12:10:27 +0100

    Fix Redux store Immutable import/export
    Previously if the store was exported using Redux Devtools,
    Immutable Maps/Lists would be converted to their JS counterparts.
    This is not the case anymore.

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,8 +1,16 @@
 import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 import rootReducer from "./reducers/rootReducer";
+import Immutable from "immutable";
 
 export default function configureStore() {
-  const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+  const composeEnhancers =
+    typeof window === "object" && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+      ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+          serialize: {
+            immutable: Immutable,
+          },
+        })
+      : compose;
   return createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
 }
