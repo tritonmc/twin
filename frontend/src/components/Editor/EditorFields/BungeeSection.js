d7c5d0d1f07b8b1ed3223d8753cfda210bf8c423
Diogo Correia <me@diogotc.com>
2020-06-07 13:19:03 +0100

    Add support for Triton schema v5
    Signed-off-by: Diogo Correia <me@diogotc.com>

--- frontend/src/components/Editor/EditorFields/BungeeSection.js
+++ frontend/src/components/Editor/EditorFields/BungeeSection.js
@@ -2,17 +2,25 @@ import React, { Component } from "react";
 import CheckboxField from "./CheckboxField";
 import Grid from "@material-ui/core/Grid";
 import ServersField from "./ServersField";
+import { connect } from "react-redux";
 
 export class MetaSection extends Component {
   render() {
-    const { id } = this.props;
+    const { id, tritonVersion } = this.props;
     return (
       <Grid container spacing={3}>
-        <Grid item xs={12} sm={6}>
-          <CheckboxField id={id} path="universal" label="Universal" default={true} />
-        </Grid>
-        <Grid item xs={12} sm={6}>
-          <CheckboxField id={id} path="blacklist" label="Use server list as blacklist" />
+        {tritonVersion < 5 && (
+          <Grid item xs={12} sm={6}>
+            <CheckboxField id={id} path="universal" label="Universal" default={true} />
+          </Grid>
+        )}
+        <Grid item xs={12} sm={tritonVersion >= 5 ? 12 : 6}>
+          <CheckboxField
+            id={id}
+            path="blacklist"
+            label="Use server list as blacklist"
+            default={tritonVersion >= 5}
+          />
         </Grid>
         <Grid item xs={12}>
           <ServersField id={id} />
@@ -22,4 +30,8 @@ export class MetaSection extends Component {
   }
 }
 
-export default MetaSection;
+const mapStateToProps = (state) => ({
+  tritonVersion: state.main.get("tritonVersion", 1),
+});
+
+export default connect(mapStateToProps)(MetaSection);
